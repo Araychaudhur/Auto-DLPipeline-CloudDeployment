da6be92ec53d766ed081466167d556bb22d0872b
Araychaudhur <araychaudhur@binghamton.edu>
2025-02-14 04:19:51 -0500

    Pipeline Implementation for base and updated model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -13,3 +14,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+        logger.info(f">>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<\n\n ------------------ ")
+    
+except Exception as e:
+        logger.exception(e)
+        raise e
+
